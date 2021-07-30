a061deb682977fa28b61bb2ff093769203044e8f
claudiaMorales2018 <cvbarreram@yahoo.com>
2021-07-30 16:33:07 -0600

    subir proyecto

---
+++ app/src/main/java/com/example/appcuento/MostrarPelis.java
@@ -0,0 +1,14 @@
+package com.example.appcuento;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MostrarPelis extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mostrar_pelis);
+    }
+}
